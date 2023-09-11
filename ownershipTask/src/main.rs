f3f7b52752fb4809bfaf9d89ed3e569933be4475
Melodi Bayraktar <101207246+MelodiBayraktar@users.noreply.github.com>
2023-09-11 21:36:47 +0300

    I created function for to concatenate  two strings and returned the result

--- ownershipTask/src/main.rs
+++ ownershipTask/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let string1 = String::from("hello");
+    let string2 = String::from(" world");
+    
+    let concatenated_strings= concatenate_strings(&string1, &string2);
+    println!("{}", concatenated_strings);
+}
+fn concatenate_strings(x: &str, y: &str)->String{
+    let mut result = String::new();
+    result.push_str(x);
+    result.push_str(y);
+    return result;
 }
